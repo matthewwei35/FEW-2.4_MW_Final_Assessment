79f4256bde5ac8895e4f0e08434c2b859e3b1d30
Matthew Wei <matthewwei35@gmail.com>
2022-03-04 18:23:42 -0800

    feat(metal): complete challenge 4

--- BandsScreen.js
+++ BandsScreen.js
@@ -1,11 +1,19 @@
 import { FlatList, View, Text } from 'react-native';
 import Item from './Item';
+import DATA from './metal.json'
 
 function BandsScreen({ navigation, data }) {
   return (
-    <View>
-      <Text>Bands</Text>
-    </View>
+    <FlatList
+      data={DATA}
+      renderItem={({ item }) => {
+        console.log(item)
+        return <Item title={`${item.band_name}`} countryOfOrigin={item.origin} numberOfFans={item.fans} yearFormed={item.formed} />
+      }}
+      keyExtractor={item => {
+        return item.id
+      }}
+    />
   )
 }
 
---
+++ Item.js
@@ -0,0 +1,39 @@
+import { View, StyleSheet, Text, TouchableHighlight } from 'react-native';
+
+const Item = ({ title, countryOfOrigin, numberOfFans, yearFormed, data, navigation }) => {
+
+  // const keys = Object.keys(data)
+
+  return (
+    <TouchableHighlight
+      style={styles.item}
+      onPress={() => navigation.navigate('Details', data)}
+    >
+      <View>
+        <Text style={styles.title}>{title}</Text>
+        <Text>{countryOfOrigin}</Text>
+        <Text>{numberOfFans}</Text>
+        <Text>{yearFormed}</Text>
+      </View>
+    </TouchableHighlight>
+  )
+}
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey'
+  },
+  title: {
+    fontSize: 32,
+  },
+  details: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+});
+
+export default Item;
