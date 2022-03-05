e75f9f38fc9ae05b3d11e032dc0ecb753b835958
Matthew Wei <matthewwei35@gmail.com>
2022-03-04 18:55:51 -0800

    feat(metal): complete challenge 6

--- StatsScreen.js
+++ StatsScreen.js
@@ -1,9 +1,40 @@
 import { Text, View, StyleSheet } from 'react-native';
+import DATA from './metal.json'
+
+internationalNumberFormat = new Intl.NumberFormat('en-US')
+
+const getTotalBands = (data) => {
+  return data.length
+}
+
+const getTotalFanCount = (data) => {
+  return data.reduce((acc, b) => acc + b.fans, 0)
+}
+
+const getTotalCountryCount = (data) => {
+  const countries = data.map(b => b.origin)
+  return new Set(countries).size
+}
+
+const getTotalActiveBands = (data) => {
+  const active = data.filter(b => b.split === '-')
+  return active.length
+}
+
+const getTotalSpilitBands = (data) => {
+  const split = data.filter(b => b.split !== '-')
+  return split.length
+}
 
 function StatsScreen({ navigation, route }) {
   return (
     <View>
-      <Text>Stats</Text>
+      <Text>Metal Stats</Text>
+      <Text>Count: {getTotalBands(DATA)}</Text>
+      <Text>Fans: {internationalNumberFormat.format(getTotalFanCount(DATA) * 1000)}</Text>
+      <Text>Countries: {getTotalCountryCount(DATA)}</Text>
+      <Text>Active: {getTotalActiveBands(DATA)}</Text>
+      <Text>Split: {getTotalSpilitBands(DATA)}</Text>
     </View>
   )
 }
